cdf6752335885708fcfbe31ffafcf22874422c45
dinamico desarrollo <dinamico.moodle@gmail.com>
2023-05-18 10:10:07 -0500

    lmpiador de registro de verificacion de correo

---
+++ cleanConfirmaEmail.php
@@ -0,0 +1,13 @@
+<?php
+require_once('../../config-ext.php');
+
+$currentDateTime = date('Y-m-d H:i:s');
+$sql = "DELETE FROM ConfirmaCodigo WHERE (verificado = 0 AND CodigoFecha < '$currentDateTime') OR (verificado = 1 AND Email IN (SELECT Email FROM User))";
+$result = $link->query($sql);
+
+if ($result === true) {
+    echo "Registros eliminados exitosamente.";
+} else {
+    echo "Error al eliminar registros: " . $link->error;
+}
+?>
