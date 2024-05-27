d489a116697eb12bfaad1a0945512b62a38ddca6
dinamicosoporte <dinamico.moodle@gmail.com>
2024-05-27 16:35:24 -0500

    cambio de visualizacion de la tabla asesores

--- admin/controllerLicenciasPrf.php
+++ admin/controllerLicenciasPrf.php
@@ -2,11 +2,12 @@
 $perfil = "Profesor";
 include("/var/www/html/moodle/config-ext.php");
 
-$query =    "SELECT User.*, Licence.Code, Licence.LicenceId, Licence.Title, Enrolment.CourseName, Enrolment.GroupCode, Enrolment.GroupKey, Enrolment.EnrolmentId, PreDocentes.asesor
+$query =    "SELECT User.*, Licence.Code, Licence.LicenceId, Licence.Title, Enrolment.CourseName, Enrolment.GroupCode, Enrolment.GroupKey, Enrolment.EnrolmentId, asesores.id_usuario
             FROM User
             LEFT JOIN Licence ON User.UserId = Licence.UserId
             LEFT JOIN Enrolment ON User.UserId = Enrolment.UserId
             LEFT JOIN PreDocentes ON User.Email = PreDocentes.email
+            LEFT JOIN asesores ON User.UserId = asesores.id_docente
             WHERE User.Rol = ?";
 
 $stmt = mysqli_prepare($link, $query);
