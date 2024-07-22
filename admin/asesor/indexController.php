77113c6ffb6bebbf937902c2f50a8c812be165f8
dinamicosoporte <dinamico.moodle@gmail.com>
2024-07-22 09:56:09 -0500

    estadisticas de plataforma en dashboard asesores

--- admin/asesor/indexController.php
+++ admin/asesor/indexController.php
@@ -38,13 +38,10 @@ if(isset($asesor)) {
         }
     }
 
-    // Contar el total de Docentes
     $total_docentes = count(array_unique(array_column($datos, 'Docente')));
-        
-    // Contar el total de Id_Estudiantes
+
     $total_estudiantes = count($datos);
-    
-    // Contar el total de Códigos de Colegio
+
     $total_colegios = count(array_unique(array_column($datos, 'Codigo_Colegio')));
     
     $docentes_porcentaje = number_format((($total_docentes / $datos[0]['Total_Asesores'])*100), 2);
@@ -52,6 +49,54 @@ if(isset($asesor)) {
     $estudiantes_porcentaje = number_format((($total_estudiantes / $datos[0]['Total_Classroom'])*100), 2);
 
     $colegios_porcentaje = number_format((($total_colegios / $datos[0]['Total_Colegios'])*100), 2);
+
+    $nivel = array();
+    $numero_nivel = array();
+    $comprobar_nivel = "";
+    $proyectos = array();
+    $libros = count($datos);
+    $proyectosHTML = "";
+    $colores = array('danger','warning','info','success','primary');
+    $coloresCont = count($colores) - 1;
+    $selectColor = 0;
+    $losColegios = array();
+
+    foreach ($datos as $valor) {
+        $curso = $valor['Curso'];
+        $colegio = $valor['Nombre_Colegio'];
+        if (array_key_exists($curso, $proyectos)) {
+            $proyectos[$curso]++;
+        } else {
+            $proyectos[$curso] = 1;
+        }
+        if (array_key_exists($colegio, $losColegios)) {
+            $losColegios[$colegio]++;
+        } else {
+            $losColegios[$colegio] = 1;
+        }
+    }
+
+    foreach ($proyectos as $curso => $cantidad) {
+        $porcentaje = $cantidad/$libros;
+        $porcentaje = number_format($porcentaje*100, 2);
+
+        if($selectColor>4){
+            $selectColor = 0;
+        }
+
+        $proyectosHTML .= '<h4 class="small font-weight-bold">'.$curso.' ('.$cantidad.') <span class="float-right">'.$porcentaje.'%</span></h4>';
+        $proyectosHTML .= '<div class="progress mb-4">';
+            $proyectosHTML .= '<div class="progress-bar bg-'.$colores[$selectColor].'" role="progressbar" style="width: '.$porcentaje.'%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>';
+        $proyectosHTML .= '</div>';
+
+        $selectColor++;
+    }
+
+    foreach ($losColegios as $colegio => $cantidadCole) {
+        $imprimirColegio = '<div class="row">
+                                <div class="col">'.$colegio.'</div>
+                            </div>';
+    }
 
 }
 ?>
