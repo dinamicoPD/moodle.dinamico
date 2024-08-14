b1698da663f0b8796efa646f0ec0250f43e13f7c
dinamicosoporte <dinamico.moodle@gmail.com>
2024-08-14 10:39:58 -0500

    oculatado seccion festivales en dashboard

--- admin/asesor/index.php
+++ admin/asesor/index.php
@@ -266,21 +266,21 @@ require('componentes.php');
 
 						<!-- Content Row -->
 						<hr>
-						
+
 						<div class="d-sm-flex align-items-center justify-content-between mb-4">
 							<h1 class="h1 mb-0 text-gray-800">Festivales</h1>
 						</div>
-						
+						<!--
 						<div class="row">
 
-							<!-- Area Chart -->
+
 							<div class="col-xl-8 col-lg-7">
 								<div class="card shadow mb-4">
-									<!-- Card Header - Dropdown -->
+
 									<div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
 										<h6 class="m-0 font-weight-bold text-primary">Festivales</h6>
 									</div>
-									<!-- Card Body -->
+
 									<div class="card-body">
 										<div class="chart-area">
 											<canvas id="myAreaChart"></canvas>
@@ -289,10 +289,10 @@ require('componentes.php');
 								</div>
 							</div>
 
-							<!-- Pie Chart -->
+
 							<div class="col-xl-4 col-lg-5">
 								<div class="card shadow mb-4">
-									<!-- Card Header - Dropdown -->
+
 									<div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
 										<h6 class="m-0 font-weight-bold text-primary">Revenue Sources</h6>
 										<div class="dropdown no-arrow">
@@ -308,7 +308,7 @@ require('componentes.php');
 											</div>
 										</div>
 									</div>
-									<!-- Card Body -->
+
 									<div class="card-body">
 										<div class="chart-pie pt-4 pb-2">
 											<canvas id="myPieChart"></canvas>
@@ -328,8 +328,9 @@ require('componentes.php');
 								</div>
 							</div>
 						</div>
+						-->
 					</div>
-					<!-- /.container-fluid -->
+					
 
 				</div>
 				<!-- End of Main Content -->
